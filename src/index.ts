2071315d48bc27ae7e1fb5e639e9bb996e0aaf9f
labyh123456 <tommmyy890@gmail.com>
2023-05-27 19:00:35 +0500

    done

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,61 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-import confetti from 'canvas-confetti';
+import { v4 as uuidV4 } from 'uuid';
+// console.log(uuidV4());
+// export {}
+type Task = {
+    id: string,
+    title: string,
+    completed: boolean,
+    createAt:Date
+}
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.querySelector<HTMLFormElement>('#new-task-form');
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const tasks: Task[] = loadTasks();
+
+tasks.forEach(addListItem)
+
+
+form?.addEventListener('submit', e => {
+    e.preventDefault();
+   
+    if (input?.value == '' || input?.value == null) return;
+    const task : Task = {
+        id: uuidV4(),
+        title: input?.value,
+        completed: false,
+        createAt: new Date()
+    }
+
+
+    addListItem(task);
+    tasks.push(task)
+    saveTasks();
+    input.value = '';
+}) 
+
+function addListItem(task: Task) {
+    const item = document.createElement('li');
+    const label = document.createElement('label');
+    const checkbox = document.createElement('input');
+    checkbox.addEventListener('change', () => {
+        task.completed = checkbox.checked;
+        saveTasks()
+    })
+    checkbox.type = "checkbox";
+    checkbox.checked = task.completed
+    label.append(checkbox, task.title);
+    item.append(label);
+    list?.append(item);
+}
+
+function saveTasks() {
+    localStorage.setItem('TASK', JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[]{
+    const taskJSON = localStorage.getItem('TASK');
+    if (taskJSON == null) return [];
+    return JSON.parse(taskJSON);
+}
